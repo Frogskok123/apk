c416601f46eb25bd2dbc6dd549b9561f522d40e8
Frogskok123 <39944364+Frogskok123@users.noreply.github.com>
2026-01-11 23:33:45 +0500

    fix: Clean up Android.mk - remove non-ASCII comments

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,25 +2,25 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# --- Настройки компилятора ---
+# Compiler settings
 LOCAL_CFLAGS := -std=c++17
 LOCAL_CFLAGS += -Wno-error=format-security -w
 LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive -fvisibility=hidden
 LOCAL_CFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
 
 LOCAL_CPPFLAGS := -std=c++17
-LOCAL_CPPFLAGS += -Wno-error=format-security -fpermissive -w -s 
+LOCAL_CPPFLAGS += -Wno-error=format-security -fpermissive -w -s
 LOCAL_CPPFLAGS += -fno-rtti -fno-exceptions -fms-extensions -Wno-error=c++11-narrowing -fvisibility=hidden
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
 
-# --- Заголовочные файлы ---
+# Header files
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/Encrypt
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/Http
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/Vulkan
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/ImGui
 
-# --- Исходные файлы ---
+# Source files
 LOCAL_SRC_FILES := \
     src/Main.cpp \
     src/JNI_Bridge.cpp \
@@ -38,12 +38,11 @@ LOCAL_SRC_FILES := \
     src/Vulkan/vulkan_wrapper.cpp \
     src/Vulkan/VulkanUtils.cpp
 
-# --- Имя библиотеки (будет libzalypa.so / liblittleHack.so) ---
+# Library name
 LOCAL_MODULE := littleHack
 
-# --- Библиотеки Android ---
-# Добавлено: -lvulkan (для вулкана), -landroid (для работы с окнами)
+# Android libraries
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM -lGLESv2 -lGLESv3 -lvulkan
 
-# --- Сборка как SHARED LIBRARY (для APK) ---
+# Build as shared library
 include $(BUILD_SHARED_LIBRARY)
